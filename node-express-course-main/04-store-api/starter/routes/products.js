9d8876abee97269f2b79080261135a6111782761
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-02-20 22:44:22 +0530

    commit

--- node-express-course-main/04-store-api/starter/routes/products.js
+++ node-express-course-main/04-store-api/starter/routes/products.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router()
-const {getAllProductsStatic , getAllProducts} = require("../controllers/products");
+const {getAllProductsStatic , getAllProducts , createProduct} = require("../controllers/products");
 
 
 router.route('/').get(getAllProducts)
 router.route('/static').get(getAllProductsStatic)
+router.route("/").post(createProduct)
 
 
 module.exports = router
