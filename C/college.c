8a7097309939bc2dad0ecacc607cec6f24537351
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-04-02 20:07:52 +0530

    learning C++

--- C/college.c
+++ C/college.c
@@ -1,20 +1,13 @@
-#include <stdio.h> 
+#include <Stdio.h>
 
 int main(){
-    int sum = 0 ; 
-    // checking perfect numbers
-    for(int i = 1 ; i <= 1000000000; i++){
-        sum = 0 ; 
-        for(int j = 1 ; j <= i / 2 ; j++){
-            if(i%j == 0){
-                sum = sum + j ; 
-            }
-        }
-        
-        // printf("i = %d , sum = %d\n" , i , sum);
-        if(sum == i ){
-            printf("%d," , i); 
-        }
+    int a  = 8 ; 
+    int b = 3 ; 
+    
+    if(a % 2 == 0 || b % 2 == 0 ){
+        printf("One or both numbers are divisible by 2");
+    }else{
+        printf("None of the above are divisble by 2");
     }
     return 0 ;
 }
