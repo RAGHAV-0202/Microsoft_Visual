6c38f8f77708f82af8a3555ce75e8ab339f465ab
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-04-09 08:03:57 +0530

    dsa day 2 , hashing

--- DSA/hashing/hash_map.cpp
+++ DSA/hashing/hash_map.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <map>
 using namespace std ;
 
 
+// storing and fetching takes log(n) under all cases
+
 int main(){
+
+    int array[] = {1,2,3,2,2,5,1,1,2,4,5,1,2,4,123542};
+
+    map <int,int> mpp ; 
+
+    for(int i = 0 ; i < sizeof(array) / sizeof(array[0]) ; i++){
+        mpp[array[i]] ++ ;
+    }
+
+    cout << mpp[1] << endl ;
+    cout << mpp[2] << endl;
+    cout << mpp[5] << endl;
 
 
     return 0 ;
