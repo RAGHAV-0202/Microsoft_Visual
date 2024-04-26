bf4512081116e0c3ed9dad653c47cdc6f347810b
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-04-26 21:49:08 +0530

    day 19

--- DSA/Problems/Step 3/hard.cpp
+++ DSA/Problems/Step 3/hard.cpp
@@ -329,6 +329,25 @@ bool isAnagram(string s, string t) {
 
 // day 19 below
 
+// #392
+bool isSubsequence(string s, string t) {
+    int last_index = -1 ;
+    for(int i = 0 ; i < s.size() ; i++){
+        int flag = 0 ; 
+        for(int j = last_index+1 ; j < t.size() ; j++){
+            if(s[i] == t[j]){
+                flag = 1 ; 
+                last_index = j ;
+                break;
+            }
+        }
+        if(flag == 0){
+            return false ;
+        }
+    }
+    return true;
+}
+
 
 int main(){
     // int x = particular_Pascal_at_r_c(5,3);
