37c7dd150c3f66be43ea08d1af1af9076682a9a7
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-05-18 23:54:45 +0530

    react

--- React/basic/business_card/src/index.js
+++ React/basic/business_card/src/index.js
@@ -1,17 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function Image(){
+  return (
+    <div className='img_div'>
+      <img className='image' src='https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745' alt="image"></img>      
+    </div>
+  )
+}
+function Details(){
+  return(
+    <div className='details'>
+        <h4>Name Surname</h4>
+        <h5>Data Scientits</h5>
+        <h6>Name.DS</h6>
+    </div>
+  )
+}
+
+
+function Socials(){
+  return (
+    <div className='socials'>
+      <div className='social_div blue'>LOGO</div>
+      <div className='social_div white'>LOGO</div>
+    </div>
+  )
+}
+function About(){
+  return (
+    <div className='about'>
+    <h6>About</h6>
+    <p>I am a person , this person might be or might not be imaginary , might be present might me absent. This person is a Person</p>
+    </div>
+  )
+}
+
+function Intro(){
+  return (
+    <div className='about'>
+    <h6>Interest</h6>
+    <p>I am a person , this person might be or might not be imaginary , might be present might me absent. This person is a Person</p>
+    </div>
+  )
+}
+function Footer(){
+  return (
+    <div className='footer'>
+      <div className='icons'></div>
+      <div className='icons'></div>
+      <div className='icons'></div>
+      <div className='icons'></div>
+    </div>
+  )
+}
+
+function App(){
+  return(
+    <div className='container'>
+      <Image/>
+      <Details/>
+      <Socials/>
+      <About/>
+      <Intro/>
+      <Footer/>
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <App/>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
